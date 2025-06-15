770c5adcb46f9e900ea0290f70d0f678441e9747
vijayvji <74865729+vijayvji@users.noreply.github.com>
2025-06-16 01:10:19 +0530

    Update XposedInit.java

--- xposedmodule/src/main/java/com/vaishu/xposed/XposedInit.java
+++ xposedmodule/src/main/java/com/vaishu/xposed/XposedInit.java
@@ -1,1 +1,14 @@
+package com.vaishu.xposed;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class XposedInit implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (lpparam.packageName.equals("com.android.systemui")) {
+            AssistantHook.hookSystemUI(lpparam);
+        }
+    }
+}
 
