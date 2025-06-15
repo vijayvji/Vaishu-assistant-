8ca017d6b25f9fe3467179177738d500e1cee96f
vijayvji <74865729+vijayvji@users.noreply.github.com>
2025-06-16 01:12:41 +0530

    Update AssistantHook.java

--- xposedmodule/src/main/java/com/vaishu/xposed/AssistantHook.java
+++ xposedmodule/src/main/java/com/vaishu/xposed/AssistantHook.java
@@ -1,1 +1,34 @@
+package com.vaishu.xposed;
+
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class AssistantHook {
+
+    public static void hookSystemUI(final XC_LoadPackage.LoadPackageParam lpparam) {
+        try {
+            Class<?> phoneWindowManagerClass = XposedHelpers.findClass(
+                    "com.android.server.policy.PhoneWindowManager", lpparam.classLoader
+            );
+
+            XposedHelpers.findAndHookMethod(phoneWindowManagerClass, "interceptKeyBeforeQueueing",
+                    android.view.KeyEvent.class, int.class, new XC_MethodHook() {
+
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            // Example: Detect long-press power button or volume to trigger custom behavior
+                            Log.d("AssistantHook", "Key intercepted by Vaishu Xposed Module");
+                        }
+                    });
+
+        } catch (Throwable t) {
+            Log.e("AssistantHook", "Failed to hook", t);
+        }
+    }
+}
 
