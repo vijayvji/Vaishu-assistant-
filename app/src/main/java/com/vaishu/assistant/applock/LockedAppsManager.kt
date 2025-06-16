2d793561cdef0b44db1379dd89b1d1d64ec5e768
vijayvji <74865729+vijayvji@users.noreply.github.com>
2025-06-17 00:27:15 +0530

    Update LockedAppsManager.kt

--- app/src/main/java/com/vaishu/assistant/applock/LockedAppsManager.kt
+++ app/src/main/java/com/vaishu/assistant/applock/LockedAppsManager.kt
@@ -1,1 +1,21 @@
+package com.vaishu.assistant.applock
+
+import android.content.Context
+
+class LockedAppsManager(context: Context) {
+
+    private val prefs = context.getSharedPreferences("LockedApps", Context.MODE_PRIVATE)
+
+    fun isLocked(packageName: String?): Boolean {
+        return prefs.getBoolean(packageName ?: "", false)
+    }
+
+    fun lockApp(packageName: String) {
+        prefs.edit().putBoolean(packageName, true).apply()
+    }
+
+    fun unlockApp(packageName: String) {
+        prefs.edit().remove(packageName).apply()
+    }
+}
 
